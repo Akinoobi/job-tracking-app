34e14daadc0c9adca6e88fc41d282b3c5b9a45b5
Akinoobi <gelo.palma23@gmail.com>
2023-02-08 23:32:37 +0800

    add: setup section 3 landing page
    Signed-off-by: Akinoobi <gelo.palma23@gmail.com>

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+  <div>
+  <h1>jobify</h1>
+  <Landing />
+  </div>
+  )
+}
+
+export default App;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,9 @@
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+const Landing = () => {
+  return (
+    <h4>Landing Page</h4>
+  )
+}
+
+export default Landing
