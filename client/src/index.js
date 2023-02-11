ba975ff1c0351bbc288bf99fb80459cd3dd7431f
Akinoobi <gelo.palma23@gmail.com>
2023-02-11 20:17:13 +0800

    chore: prepare register & login frontend & reducer
    Signed-off-by: Akinoobi <gelo.palma23@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
 import App from './App';
-
+import { AppProvider } from './context/appContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AppProvider>
     <App />
+    </AppProvider>
   </React.StrictMode>
 );
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,37 +1,75 @@
-import Wrapper from "../assets/wrappers/RegisterPage"
-import { useState } from "react"
-import { FormRow, Logo } from "../components"
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useState } from "react";
+import { FormRow, Logo, Alert } from "../components";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
-    name: '',
-    email: '',
-    password: '',
-    isMember: true,
-}
+  name: "",
+  email: "",
+  password: "",
+  isMember: false,
+};
 const Register = () => {
-    const [values, setValues] = useState(initialState)
-    // global state and useNavigate
-
-    const handleChange = (e) => {
-        console.log("e", e.target)
+  const [values, setValues] = useState(initialState);
+  const { isLoading, showAlert, displayAlert } = useAppContext();
+  // global state and useNavigate
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const {name, email, password, isMember} = values
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert()
+      return
     }
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log(e.target)
-    }
-    return (
-        <Wrapper className="full-page">
-            <form className="form" onSubmit={onSubmit}>
-                <Logo />
-                <h3>Login</h3>
-                {/* name input */}
-                <FormRow/>
-                <button type='submit' className="btn btn-block">
-                    submit
-                </button>
-            </form>
-        </Wrapper>
-    )
-}
+    console.log(values)
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert />}
+        {/* name input */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
 
-export default Register
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
