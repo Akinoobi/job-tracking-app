defdb5a5a1da6fce0d3bf74fcd0141e072c2ab00
Akinoobi <gelo.palma23@gmail.com>
2023-02-16 16:50:50 +0800

    chore: connect frontend and backend register
    Signed-off-by: Akinoobi <gelo.palma23@gmail.com>

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useContext } from "react";
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import { DISPLAY_ALERT, CLEAR_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_SUCCESS, REGISTER_USER_ERROR } from "./actions";
+import axios from 'axios'
 const initialState = {
   isLoading: false,
   showAlert: false,
@@ -9,6 +10,7 @@ const initialState = {
   user: null,
   token: null,
   userLocation: '',
+  jobLocation: '',
 };
 
 const AppContext = React.createContext();
@@ -24,12 +26,26 @@ const AppProvider = ({ children }) => {
   const clearAlert = () => {
     setTimeout(() => {
       setTimeout(() => {
-        dispatch({ CLEAR_ALERT });
+        dispatch({ type: CLEAR_ALERT });
       }, 3000);
     });
   };
   const registerUser = async (currentUser) => {
-    console.log(currentUser)
+    dispatch({ type: REGISTER_USER_BEGIN });
+    try {
+      const response = await axios.post('/api/v1/auth/register', currentUser)
+      console.log(response);
+      const {user, token, location} = response.data
+      dispatch({type: REGISTER_USER_SUCCESS, payload: { user, token, location}})
+      // local storage
+    } catch (error) {
+      console.log("error", error.response)
+      dispatch({
+        type: REGISTER_USER_ERROR, 
+        payload: { msg: error.response.data.msg }
+      })
+    }
+    clearAlert();
   }
   return (
     <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
