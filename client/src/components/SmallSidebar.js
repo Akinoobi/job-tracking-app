a542dda437e2ba409220e44fe326d5c53f2a4fa3
Akinoobi <gelo.palma23@gmail.com>
2023-02-19 23:19:08 +0800

    chore: show mobile side bar
    Signed-off-by: Akinoobi <gelo.palma23@gmail.com>

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,11 +1,43 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
-
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 const SmallSideBar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-    <div>SmallSideBar</div>
+      <div className={`sidebar-container ${showSidebar ? "show-sidebar" : ""}`}>
+        <div className="content">
+          <button className="close-btn" onClick={() => toggleSidebar()}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={toggleSidebar}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SmallSideBar
+export default SmallSideBar;
